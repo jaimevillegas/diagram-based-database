87517f18799418148e5e83bb8531cd6af965a0dc
David Espino <davidespinog@gmail.com>
2023-05-08 15:34:32 -0600

    Add indexes and chante DATETIME

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -8,8 +8,8 @@ CREATE TABLE patients (
 CREATE TABLE invoices(
   id INT NOT NULL AUTO_INCREMENT,
   total_amount DECIMAL(10,2) NOT NULL,
-  generated_at DATETIME NOT NULL,
-  payed_at DATETIME,
+  generated_at TIMESTAMP NOT NULL,
+  payed_at TIMESTAMPS,
   medical_history_id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
@@ -17,7 +17,7 @@ CREATE TABLE invoices(
 
 CREATE TABLE medical_histories(
   id INT NOT NULL AUTO_INCREMENT,
-  admitted_at DATETIME NOT NULL,
+  admitted_at TIMESTAMP NOT NULL,
   patient_id INT NOT NULL,
   status VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
@@ -30,8 +30,10 @@ CREATE TABLE invoice_items(
   quantity INT NOT NULL,
   total_price DECIMAL(10,2) NOT NULL,
   invoice_id INT NOT NULL,
+  treatment_id INT NOT NULL, 
   PRIMARY KEY (id),
   FOREIGN KEY (invoice_id) REFERENCES invoices(id)
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
 CREATE TABLE treatments(
@@ -51,4 +53,6 @@ CREATE TABLE medical_histories_treatments(
 CREATE INDEX index_invoices ON invoices (medical_history_id);
 CREATE INDEX index_medical_histories ON medical_histories (patient_id);
 CREATE INDEX index_invoice_items ON invoice_items (invoice_id);
-CREATE INDEX index_med_hist_treatments ON medical_histories_treatments (medical_history_id);
+CREATE INDEX index_invoice_treatments ON invoice_items (treatment_id);
+CREATE INDEX index_med_hist_medical_history ON medical_histories_medical_history (medical_history_id);
+CREATE INDEX index_med_hist_treatments ON medical_histories_treatments (treatment_id);
